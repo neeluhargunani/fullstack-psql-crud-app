76d910dd179f1e58c725e831393f5a46be03c6e7
neelu <neelamh1000@gmail.com>
2021-05-30 21:24:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-   user:"rkpolnfemcoiex",
-    password:"c7627581880e947aad913b03b5f3f09c4eddcb783603d7c9563fd26ae2e018af",
-    host:"ec2-3-212-75-25.compute-1.amazonaws.com",
+ user:"zwkcdsxeyziptf",
+    password:"476a8741200bca2b6b1850f1d16eb3c09b6468e950149a00a527712ba1ae6015",
+    host:"ec2-54-243-92-68.compute-1.amazonaws.com",
     port:5432,
-    database:"da85e7t5v78frf",
-    URI:"postgres://rkpolnfemcoiex:c7627581880e947aad913b03b5f3f09c4eddcb783603d7c9563fd26ae2e018af@ec2-3-212-75-25.compute-1.amazonaws.com:5432/da85e7t5v78frf"
+    database:"ddrtssbkilquim",
+    URI:"postgres://zwkcdsxeyziptf:476a8741200bca2b6b1850f1d16eb3c09b6468e950149a00a527712ba1ae6015@ec2-54-243-92-68.compute-1.amazonaws.com:5432/ddrtssbkilquim"
    
 });
 module.exports = pool;
