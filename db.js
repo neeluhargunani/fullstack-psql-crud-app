9069c30db3c371a2cdb9d42f047426935cc347bb
neelam hargunani <neelamh1000@gmail.com>
2021-05-30 21:07:52 +0530

    make it better

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user:"postgres",
-    password:"webcreationz@1234",
-    host:"localhost",
+    user:"rkpolnfemcoiex",
+    password:"c7627581880e947aad913b03b5f3f09c4eddcb783603d7c9563fd26ae2e018af",
+    host:"ec2-3-212-75-25.compute-1.amazonaws.com",
     port:5432,
-    database:"student_form"
+    database:"da85e7t5v78frf",
+    URI:"postgres://rkpolnfemcoiex:c7627581880e947aad913b03b5f3f09c4eddcb783603d7c9563fd26ae2e018af@ec2-3-212-75-25.compute-1.amazonaws.com:5432/da85e7t5v78frf"
+   
 });
 module.exports = pool;
